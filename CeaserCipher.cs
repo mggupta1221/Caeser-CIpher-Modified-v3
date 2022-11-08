701b89706e4e45d06e9d02af17de2bc298acf9aa
Mukesh Gupta <Mukesh.Gupta@Klingelnberg.com>
2022-11-08 17:47:01 +0530

    Abstraction

---
+++ CeaserCipher.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text;
+
+namespace CaeserCipherAlgorithm
+{
+    public class CaeserCipher
+    {
+        private StringBuilder cipheredText = new StringBuilder();
+
+       
+
+        private bool IsAlphabet(int alphabetsAsciiValue)
+        {
+            return ((IsCapitalAlphabet(alphabetsAsciiValue) || IsSmallAlphabet(alphabetsAsciiValue)));
+        }
+        private bool IsCapitalAlphabet(int alphabetsAsciiValue)
+        {
+            return (alphabetsAsciiValue >= 65 && alphabetsAsciiValue <= 90);
+        }
+        private bool IsSmallAlphabet(int alphabetsAsciiValue)
+        {
+            return (alphabetsAsciiValue >= 97 && alphabetsAsciiValue <= 122);
+        }
+        public StringBuilder DoCeaserCipher(string inputString, int shiftingFactor) 
+        {
+            char alphabetToBeAdded;
+            for (int count = 0; count < inputString.Length; count++)
+            {
+                int alphabetsAsciiValue = (int)(inputString[count]);
+                if (IsAlphabet(alphabetsAsciiValue))
+                {
+                    int alphabetsAsciiValueWithShiftinFactor = alphabetsAsciiValue + shiftingFactor;
+                    if ((IsCapitalAlphabet(alphabetsAsciiValueWithShiftinFactor) && IsCapitalAlphabet(alphabetsAsciiValue)) || ((IsSmallAlphabet(alphabetsAsciiValueWithShiftinFactor) && IsSmallAlphabet(alphabetsAsciiValue))))
+                    {
+                        char cipheredCharacter = (char)alphabetsAsciiValueWithShiftinFactor;
+                        cipheredText.Append(cipheredCharacter);
+                    }
+                    else
+                    {
+                        if (shiftingFactor > 0)
+                        {
+                            if (IsCapitalAlphabet(alphabetsAsciiValue))
+                            {
+                               alphabetsAsciiValueWithShiftinFactor = 64 + (alphabetsAsciiValueWithShiftinFactor - 90);
+                                alphabetToBeAdded = (char)alphabetsAsciiValueWithShiftinFactor;
+                                cipheredText.Append(alphabetToBeAdded);
+                            }
+                            else if (IsSmallAlphabet(alphabetsAsciiValue)) 
+                            {
+                                alphabetsAsciiValueWithShiftinFactor = 96 + (alphabetsAsciiValueWithShiftinFactor - 122);
+                                alphabetToBeAdded = (char)alphabetsAsciiValueWithShiftinFactor;
+                                cipheredText.Append(alphabetToBeAdded);
+                            }
+                        }
+                        else
+                        {
+                            if (IsCapitalAlphabet(alphabetsAsciiValue))
+                            {
+                                if (alphabetsAsciiValueWithShiftinFactor < 65)
+                                {
+                                   alphabetsAsciiValueWithShiftinFactor = 90 - (64 - alphabetsAsciiValueWithShiftinFactor);
+                                }
+                                alphabetToBeAdded = (char)alphabetsAsciiValueWithShiftinFactor;
+                                cipheredText.Append(alphabetToBeAdded);
+                            }
+                            else if (IsSmallAlphabet(alphabetsAsciiValue) && alphabetsAsciiValueWithShiftinFactor < 97) 
+                            {
+                               alphabetsAsciiValueWithShiftinFactor = 122 - (96 - alphabetsAsciiValueWithShiftinFactor);
+                                alphabetToBeAdded = (char)alphabetsAsciiValueWithShiftinFactor;
+                                cipheredText.Append(alphabetToBeAdded);
+                            }
+                        }
+
+                    }
+                }
+                else
+                {
+                    cipheredText.Append(inputString[count]);
+                }
+            }
+            return cipheredText;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using CaeserCipherAlgorithm;
+using System.Text;
+
+public class Program{
+    public static void Main()
+    {
+        try
+        {
+            Console.Write("Enter text:");
+            string inputString = Console.ReadLine();
+            Console.Write("Enter Shifting factor:");
+            bool isValidShiftingFactor=int.TryParse(Console.ReadLine(), out int shiftingFactor);
+
+            if (isValidShiftingFactor)
+            {
+                CaeserCipher ceaserCipher = new CaeserCipher();
+                StringBuilder cipheredText = ceaserCipher.DoCeaserCipher(inputString, shiftingFactor);
+                Console.WriteLine("Ciphered Text:" + cipheredText); 
+            }
+            else
+            {
+                Console.WriteLine("Shifting Factor is not A Number");
+                throw new Exception("Shifting Factor is not A Number");
+                
+            }
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine(ex.Message);
+            Console.WriteLine();
+        }
+        catch (NullReferenceException ex1)
+        {
+            Console.WriteLine(ex1.Message);
+            Console.WriteLine();
+        }
+    }
+            
+}
